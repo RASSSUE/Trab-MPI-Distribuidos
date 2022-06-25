855261093f52efb41b3d9e70cbe0180c5e17501e
Gabriel Junges Baratto <gabrieljbaratto@gmail.com>
2022-06-25 18:25:10 -0300

    🔥 modificando serial para trabalhar com matrizes de uma dimensão

--- serial.c
+++ serial.c
@@ -1,61 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM 2000
+
+#define NUM 10
+#define MX_TYPE long double // tipo dos elementos da matriz
+#define ID_TYPE long int // tipo dos índices da matriz
 
 int main()
 {
   
   // criando e inicializando as matrizes 1, 2 e 3 (a matriz 3 inicializada com zeros):
+  printf("\n*versão serial*\n\n");
+  printf("criando as matrizes 1, 2 e 3...");
 
-  long double **m1,**m2,**m3;
-  int i, j, k;
-  m1= malloc(NUM * sizeof(long double *));
-  m2= malloc(NUM * sizeof(long double *));
-  m3= malloc(NUM * sizeof(long double *));
+  MX_TYPE   *m1_1d,
+            *m2_1d,
+            *m3_1d;
 
-  for(i=0;i<NUM;i++){
-    m1[i] = malloc(NUM * sizeof(long double));
-  }
-  for(i=0;i<NUM;i++){
-    m2[i] = malloc(NUM * sizeof(long double));
-  }
-  for(i=0;i<NUM;i++){
-    m3[i] = calloc(NUM,sizeof(long double));
-  }
+  ID_TYPE i, j, k;
+
+  m1_1d= malloc(NUM * NUM * sizeof(MX_TYPE));
+  m2_1d= malloc(NUM * NUM * sizeof(MX_TYPE));
+  m3_1d= calloc(NUM * NUM, sizeof(MX_TYPE));
+  printf("OK\n");
 
   // inicializando os valores da matriz 1 e 2 (utilizando os valores dos índices)
+  printf("inicializando matrizes 1 e 2...");
 
   for(i = 0; i<NUM; i++){
     for(j = 0; j<NUM; j++){
-      m1[i][j]=i;
-      m2[i][j]=j;
+      m1_1d[NUM*i+j] = i;
+      m2_1d[NUM*i+j] = j;
     }
   }
+  printf("OK\n");
 
   // multiplicando matrizes 1 e 2, salvando resultado na matriz 3
+  printf("multiplicando matrizes 1 e 2...");
 
   for(i=0;i<NUM;i++){
     for(j=0;j<NUM;j++){
       for(k=0;k<NUM;k++){
-        m3[i][j] += m1[i][k] * m1[k][j];
-       }
+        m3_1d[NUM*i+j] += m1_1d[NUM*i+k] * m2_1d[NUM*k+j];
+      }
     }
   }
+  printf("OK\n");
+
+  // salvando a matriz 3 em um arquivo para conferência no código paralelo
+  printf("salvando matriz resultado no arquivo 'mx.data'...");
+
+  int written = 0;
+  FILE *f = fopen("mx.data", "wb");
+  written = fwrite(m3_1d, sizeof(MX_TYPE), NUM*NUM, f);
+  if (written == 0){
+    printf("erro durante escrita para o arquivo !\nabortando...\n");
+    return -1;
+  }
+  else printf("OK\n");
+  fclose(f);
 
   // desalocando memória das matrizes:
+  printf("\ndesalocando memória...");
 
-  for(i=0; i<NUM; i++){
-    free(m1[i]);
-  }
-  for(i=0; i<NUM; i++){
-    free(m2[i]);
-  }
-  for(i=0; i<NUM; i++){
-    free(m3[i]);
-  }
-  free(m1);
-  free(m2);
-  free(m3);
+  free(m1_1d);
+  free(m2_1d);
+  free(m3_1d);
+  printf("OK\n");
 
   return 0;
 }
